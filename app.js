ea58267a3bce90a6199b4a63f62712263b9682f3
jay719 <javariab17@gmail.com>
2021-05-19 15:44:33 -0600

    two failing tests

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ function sum(firstNumber, secondNumber){
     return firstNumber + secondNumber
 }
 
-module.exports = {
-    sum
+function subtract (firstNum, secondNum){
+    return firstNum - secondNum
+}
+
+module.exports = { //to export it
+    sum,
+    subtract
 }
 
 console.log("It can sum 1 and 2", sum(1,2) === 3) 
--- test/app.spec.js
+++ test/app.spec.js
@@ -1,6 +1,15 @@
-const { sum } = require("../app") //desstructuring
+const { sum, subtract } = require("../app") //desstructuring
 const assert = require("assert") //built in
+const { strictEqual } = require("assert")
+
 
 it("it adds 2 and 3" , () => {
-    assert.equal(sum(2,3), 5)
+    assert.equal(sum(2,3), 6) // dont use assert
+})
+it("it takes strings", () => {
+    strictEqual(sum("2"+"3"), 5)
+})
+
+it("it subtracts 2 from 3", () => {
+    strictEqual(subtract(3,2), 1)
 })
