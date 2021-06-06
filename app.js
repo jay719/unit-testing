912c6770f8b02a379e193265c2be1922b54b7b4e
jay719 <javariab17@gmail.com>
2021-06-05 23:21:57 -0600

    changed number

--- app.js
+++ app.js
@@ -6,9 +6,17 @@ function subtract (firstNum, secondNum){
     return firstNum - secondNum
 }
 
+function incrementAge(people){
+    return people.map(person => {
+        person.age+=1
+        return person
+    })
+}
+
 module.exports = { //to export it
     sum,
-    subtract
+    subtract,
+    incrementAge
 }
 
 console.log("It can sum 1 and 2", sum(1,2) === 3) 
--- test/app.spec.js
+++ test/app.spec.js
@@ -1,4 +1,4 @@
-const { sum, subtract } = require("../app") //desstructuring
+const { sum, subtract, incrementAge } = require("../app") //desstructuring
 const assert = require("assert") //built in
 const { strictEqual } = require("assert")
 
@@ -15,5 +15,5 @@ it("subtracts 2 from 3", () => {
 })
 
 it("adds .2 and .3 ", () => {
-    assert.equal(sum(.2,.3), .5)
+    assert.equal(sum(.2,.3), .2)
 })
